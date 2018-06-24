775ac8a26b3975a1a54d7c00f88da81a28dfb62b
Mathias Deremer-Accettone <mathias.deracc@gmail.com>
2018-06-24 15:07:21 +0200

    fix(front): init

---
+++ src/components/Task.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="task">
+    <div class="todo">
+      <div v-if="task.priority == '3'" class="content-priority-hight">
+        {{ task.title }}
+      </div>
+      <div v-else-if="task.priority == '2'" class="content-priority-medium">
+        {{ task.title }}
+      </div>
+      <div v-else="task.priority == '1'" class="content-priority-low">
+        {{ task.title }}
+      </div>
+    </div>
+    <button v-on:click="deleteTask()" class="close">X</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['task'],
+
+    methods : {
+      deleteTask() {
+        var self = this;
+        this.$emit('delete', self.task.id)
+      }
+    }
+  }
+</script>
+
+<style>
+  .todo {
+    width: 250px;
+    max-width: 250px;
+    height: 40px;
+    margin-bottom: 5px;
+    display: table;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .close {
+    border: none;
+    background: none;
+    height: 40px;
+    font-weight: bold;
+  }
+
+  .task {
+    display: inline-flex;
+  }
+
+  .content-priority-hight {
+    display: table-cell;
+    vertical-align: middle;
+    padding: 5px;
+    background-color: red;
+    border: solid darkred;
+    font-weight: bold;
+    color: white;
+    opacity: 0.75;
+  }
+
+  .content-priority-low {
+    display: table-cell;
+    vertical-align: middle;
+    padding: 5px;
+    background-color: green;
+    border: solid darkgreen;
+    font-weight: bold;
+    color: white;
+    opacity: 0.75;
+  }
+
+  .content-priority-medium {
+    display: table-cell;
+    vertical-align: middle;
+    padding: 5px;
+    background-color: orange;
+    border: solid darkorange;
+    font-weight: bold;
+    color: white;
+    opacity: 0.75;
+  }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div v-for="todo in todos">
+      <task :task="todo" v-on:delete="deleteTask"></task>
+    </div>
+    <button v-on:click="showPopup()" class="button">+</button>
+    <vodal :show="show" animation="rotate" @hide="show = false">
+      <div class="form">
+        <div class="form-line">
+          Titre : <input type="text" v-model="selectedTitle" maxlength="40"><br/>
+        </div>
+        <div class="form-line">
+          Priorité :
+          <select v-model="selectedPriority">
+            <option value='1'>basse</option>
+            <option value='2'>moyenne</option>
+            <option value='3'>haute</option>
+          </select>
+        </div>
+      </div>
+      <button class="button" @click="addTask()">Ok</button>
+      <button class="cancel" @click="show = false">Annuler</button>
+    </vodal>
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+import axios from 'axios';
+import Vodal from 'vodal';
+import 'vodal/rotate.css';
+import 'vodal/common.css';
+export default {
+  name: 'TodoList',
+  components: {Task, Vodal},
+  data () {
+    return {
+      msg: 'The Todo List',
+      show: false,
+      todos: [],
+      selectedPriority: '1',
+      selectedTitle: ''
+    }
+  },
+
+  methods: {
+    showPopup() {
+      this.show = true
+    },
+
+    addTask() {
+      this.show = false,
+      axios.post('http://localhost:8082/todos', {
+        'title': this.selectedTitle,
+        'priority': this.selectedPriority
+      }).then(response => {})
+        .catch(e => {console.log(e)})
+      this.selectedPriority = 1;
+      this.selectedTitle = '';
+      window.location.href = "http://localhost:8080"
+    },
+
+    deleteTask(id) {
+      axios.delete('http://localhost:8082/todos/'+id)
+      window.location.href = "http://localhost:8080"
+    }
+  },
+
+  mounted() {
+    axios.get('http://localhost:8082/todos')
+      .then(response => {
+        this.todos = response.data
+      })
+      .catch(e => { console.log(e)})
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+  .button {
+    border-radius: 4px;
+    border:none;
+    background-color: #318CE7;
+    width: 50px;
+    height: 25px;
+    color: white;
+  }
+  .cancel {
+    border-radius: 4px;
+    border-color: #318CE7;
+    border-width: 1px;
+    background-color: white;
+    width: 75px;
+    height: 25px;
+    color: #318CE7;
+  }
+
+  .form {
+    margin-top: 50px;
+    margin-bottom: 40px;
+    text-align:left;
+    margin-left: 70px;
+  }
+
+  .form-line {
+    margin-bottom: 10px;
+  }
+
+  vodal {
+    width: 350px;
+    height: 200px;
+  }
+
+</style>
